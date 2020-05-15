f7faea79a17e395a2f4bd91f8d0304c412a935ce
snowiow <marcel.patzwahl+github@posteo.de>
2020-05-15 16:51:43 +0200

    move bastion-host-forward-props in another file

---
+++ lib/bastion-host-forward-props.ts
@@ -0,0 +1,14 @@
+import { BastionHostForwardBaseProps } from './bastion-host-forward-base-props';
+
+/** @internal */
+export interface BastionHostForwardProps extends BastionHostForwardBaseProps {
+  /**
+   * The address of the service to forward to
+   */
+  readonly address: string;
+
+  /**
+   * The port of the service to forward to
+   */
+  readonly port: string;
+}
